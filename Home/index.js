0042e7c9ff23633704249cd79b1dc5e8d3bc9a45
unknown <juliadancona8@gmail.com>
2025-08-15 21:15:07 -0300

    subindo rep

---
+++ Home/index.js
@@ -0,0 +1,18 @@
+var radio = document.querySelector('.manual-btn')
+var cont = 1
+
+document.getElementById('radio1').checked = true
+
+setInterval(() => {
+    proximovideo()
+}, 5000)
+
+function proximovideo(){
+    cont++
+
+    if(cont > 2){
+        cont = 1
+    }
+
+    document.getElementById('radio' + cont).checked = true
+}
